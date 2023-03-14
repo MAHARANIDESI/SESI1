b580f610412737ffafdd831d4eebc50c45e72664
MAHARANIDESI <127837069+MAHARANIDESI@users.noreply.github.com>
2023-03-14 17:03:20 +0800

    SESI1

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>latihan html sesi 1</title>
+</head>
+<body>
+ 
+     <h3>belajar web programming<?="<sup>80</sup>"?></h3>
+     <h3>mari kita bersama sama belajar</h3>
+
+    <script>
+        document.write("<h3>menggunakan script javascrivt</h3>");
+    </script>
+
+<?php
+   echo "<h3> menggunakan script php </h3>";
+?>
+
+</body>
+</html>
